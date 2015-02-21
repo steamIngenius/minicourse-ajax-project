c580d276c7a85a35a06bad2817d7e5a152f8b026
Micah Hawkins <micahbhawkins@gmail.com>
2015-02-21 00:30:12 -0800

    NYTimes articles now update and display properly

--- js/script.js
+++ js/script.js
@@ -22,12 +22,33 @@ function loadData() {
 
     var URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?";
     var KEY = "api-key=ebd9e23f94efc45658eb7e829bee79d1:1:71392649";
-    var URI = URL + KEY;
+    var QUERY = 'q=' + $('#city').val();
+    var URI = URL + QUERY + '&' + KEY;
 
+    // AJAJ request action, now with 50% more callback!
     $.getJSON(URI, function(data) {
-        console.log(data);
+
+        // console.log(data);
+
+        // loop through data.response.docs
+        for (var item in data.response.docs) {
+
+            // grab data.response.docs.X.web_url, data.response.docs.X.snippet and data.response.docs.X.headline.main
+            var articleLink = data.response.docs[item].web_url;
+            var articleHeadline = data.response.docs[item].headline.main;
+            var articleSnippet = data.response.docs[item].snippet;
+
+            // append the data to $nytElem unordered list
+            $nytElem.append('<li class="article"> '
+                + '<a href="' + articleLink + '">' + articleHeadline + '</a>'
+                + '<p>' + articleSnippet + '</p>'
+                + '</li>');
+        }
+
+        // FUTURE: use data.response.docs.X.lead_paragraph to implement a read more... option that stays on this page
     });
 
+    // done
     return false;
 };
 
